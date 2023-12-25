66c4d60494d97291a98d1185aee3489db122e334
Bipin Kasaju <bipin.kasaju@gmail.com>
2023-12-25 12:30:32 +0545

    Hello

---
+++ Login Test.cy.js
@@ -0,0 +1,36 @@
+///reference type="cypress"/>
+
+
+describe('Google Test', () => {
+  it('Valid credentials', () => {
+
+    cy.visit('https://books.dhoon.co/')
+    cy.get('#loginForm_email').type('bipin@dhoon.co')
+    cy.get('#loginForm_password').type('bipin666')
+    cy.contains('Login').click()
+
+
+  })
+
+
+  it('Invalid email', () => {
+
+    cy.visit('https://books.dhoon.co/')
+    cy.get('#loginForm_email').type('bipin1@dhoon.co')
+    cy.get('#loginForm_password').type('bipin666')
+    cy.contains('Login').click()
+    cy.contains('Request failed with status code 500').and('be.visible')
+
+  })
+
+  it('Invalid password', () => {
+
+    cy.visit('https://books.dhoon.co/')
+    cy.get('#loginForm_email').type('bipin@dhoon.co')
+    cy.get('#loginForm_password').type('bipin000')
+    cy.contains('Login').click()
+    cy.contains('Request failed with status code 500').and('be.visible')
+
+  })
+
+})
